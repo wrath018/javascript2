6758b5f85d693039f3a36a0a52c04b81d002c55e
Puneet Sharma <puneetsharma072000@gmail.com>
2022-05-05 12:38:38 +0530

    i

---
+++ firstQuestion.js
@@ -0,0 +1,10 @@
+function change(){
+    var name=document.getElementsByName('res0').value
+    document.getElementsByName('res01').value=name;
+    var dob=document.getElementsByName('res1').value
+    // document.getElementById('11').innerHTML=dob;
+    // var clas=document.getElementById('03').value;
+    // document.getElementById('11').innerHTML=clas;
+    // var pho=document.getElementById('04').value;
+    // document.getElementById('11').innerHTML=pho;
+}
